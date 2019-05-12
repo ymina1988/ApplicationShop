ba03e699d86ccf78ef5c4a1af0482de9a81f524d
Mina Yazdani <mina.yazdanii@gmail.com>
2019-05-13 00:51:02 +0430

    add register and login

--- ApplicationShop/ApplicationShop/Models/BLL/Main/AccountBLL.cs
+++ ApplicationShop/ApplicationShop/Models/BLL/Main/AccountBLL.cs
@@ -1,8 +1,10 @@
 ﻿using ApplicationShop.Models.DataBase;
+using ApplicationShop.Models.ViewModel.Main;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Utilities.DateTime;
 
 namespace ApplicationShop.Models.BLL.Main
 {
@@ -13,6 +15,65 @@ namespace ApplicationShop.Models.BLL.Main
             using (ApplicationShopEntities _Db = new ApplicationShopEntities())
             {
                 return 0;
+            }
+        }
+
+        public bool ChekkEmail(string Email)
+        {
+            using (ApplicationShopEntities _Db = new ApplicationShopEntities())
+            {
+                return _Db.Users.AsNoTracking().Any(b => b.Email.ToLower().Trim() == Email.ToLower().Trim());
+            }
+        }
+
+        public long Register(RegisterVM model)
+        {
+            try
+            {
+                using (ApplicationShopEntities _Db = new ApplicationShopEntities())
+                {
+                    var user = new Users
+                    {
+                        UserName = model.Email.ToLower().Trim(),
+                        Password = model.Password,
+                        Name = model.Name.ToLower().Trim(),
+                        Family = model.Family.ToLower().Trim(),
+                        phoneNumber = model.Mobile.ToLower().Trim(),
+                        Email = model.Email.ToLower().Trim(),
+                        IsActive = true,
+                    };
+                     user.UserToRoll.Add(new UserToRoll() { IdRoll = 2 });
+                    _Db.Users.Add(user);
+                    if (_Db.SaveChanges() > 0)
+                    {
+                        return user.Id;
+                    }
+                    else
+                    {
+                        return 0;
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                return 0;
+            }
+        }
+
+        public long Login(string Email, string Password)
+        {
+            using (ApplicationShopEntities _Db = new ApplicationShopEntities())
+            {
+                string P = Password;
+                var t = _Db.Users.AsNoTracking().FirstOrDefault(b => b.Email.ToLower().Trim() == Email.ToLower().Trim() && b.Password == P && b.IsActive == true);
+                if (t == null)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return t.Id;
+                }
             }
         }
     }
---
+++ ApplicationShop/ApplicationShop/Models/ViewModel/Main/AccountViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ApplicationShop.Models.ViewModel.Main
+{
+    public class RegisterVM
+    {
+        public RegisterVM()
+        {
+
+        }
+
+        [Required(ErrorMessage = "لطفا نام را وارد کنید")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "لطفا نام خانوادگی را وارد کنید")]
+        public string Family { get; set; }
+
+
+        [Required(ErrorMessage = "لطفا موبایل را وارد کنید")]
+        public string Mobile { get; set; }
+
+        [Required(ErrorMessage = "لطفا ایمیل را وارد کنید")]
+        [EmailAddress(ErrorMessage = "ایمیل معتبر نیست")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "لطفا پسورد را وارد کنید")]
+        [StringLength(255, ErrorMessage = "پسورد حداقل 6 کارکتر باشد", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+
+    public class LoginVM
+    {
+        public LoginVM()
+        {
+
+        }
+        [Required(ErrorMessage = "لطفا ایمیل را وارد کنید")]
+        [EmailAddress(ErrorMessage = "ایمیل معتبر نیست")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "لطفا پسورد را وارد کنید")]
+        [StringLength(255, ErrorMessage = "پسورد حداقل 6 کارکتر باشد", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [Display(Name = "حاصل جمع")]
+        public string Captcha { get; set; }
+    }
+}
